b6a78083e5ab6796c55c0c3a37fd60dfd6b2a944
isaac-cinza <isaaccinza730@gmail.com>
2026-09-12 22:22:45 +0200

    This is the last and complete version of my WO1-E1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Hello World! This is the Exercise1 Project.");
         Console.WriteLine("........................................");
         Console.WriteLine("What is your first name?");
         string firstName = Console.ReadLine();
